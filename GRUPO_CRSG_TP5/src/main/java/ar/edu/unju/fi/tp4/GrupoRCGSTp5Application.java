ab9ffbbc9148ffded2b26a8671c3b189924f69ba
sebas <sebas@LAPTOP-PALU9LV1>
2021-05-06 18:09:20 -0300

    task : nuestro proyecto

--- GRUPO_CRSG_TP5/src/main/java/ar/edu/unju/fi/tp4/GrupoRCGSTp5Application.java
+++ GRUPO_CRSG_TP5/src/main/java/ar/edu/unju/fi/tp4/GrupoRCGSTp5Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GrupoRCGSTp5Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GrupoRCGSTp5ApplicationTests.class, args);
+		SpringApplication.run(GrupoRCGSTp5Application.class, args);
 	}
 
 }
