6a738969c25240aa8cfad2e7a5a1205628f66509
Cristian Michel <cristianmichel33@gmail.com>
2021-05-07 19:08:19 -0300

    Creacion de la ArrayList de Compra

---
+++ GRUPO_CRSG_TP5/src/main/java/ar/edu/unju/fi/tp4/util/TablaCompra.java
@@ -0,0 +1,9 @@
+package ar.edu.unju.fi.tp4.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TablaCompra {
+	//public static List<Compra> listaCompras = new ArrayList<>();
+
+}
