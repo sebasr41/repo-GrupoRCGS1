6f044aad5d1fdc6fcc95b58666cfb85f0acf4537
Cristian Michel <cristianmichel33@gmail.com>
2021-05-07 18:54:27 -0300

    Arreglando algunos errores y agregando los LOGS

--- GRUPO_CRSG_TP5/src/main/java/ar/edu/unju/fi/tp4/service/imp/CompraServiceImp.java
+++ GRUPO_CRSG_TP5/src/main/java/ar/edu/unju/fi/tp4/service/imp/CompraServiceImp.java
@@ -1,14 +1,17 @@
 package ar.edu.unju.fi.tp4.service.imp;
 
 import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
 import ar.edu.unju.fi.tp4.service.ICompraService;
 
 @Service
 public class CompraServiceImp implements ICompraService{
-
-
+	
+	private static final Log LOGGER = LogFactory.getLog(CompraServiceImp.class);
 	//private List<Compra> compras;
 
 	@Override
@@ -16,7 +19,7 @@ public class CompraServiceImp implements ICompraService{
 		// TODO Auto-generated method stub
 
 		//compras = TablaCompras.listaCompras;
-		//compras.add(new Compra(1,"Limon",1000,2000));
+		//compras.add(new Compra(1,new Producto(5,"Samsung J5",10000,"Samsung",10 ),5,50000));
 
 	}
 
@@ -29,6 +32,10 @@ public class CompraServiceImp implements ICompraService{
 			generarTablaLCompra();
 		}
 		compras.add(compra);
+		
+		LOGGER.info("SERVICE: CompraServiceImp");
+		LOGGER.info("METHOD: guardarCompra - se agregó un objeto compra en la lista -> " + compras.get(compras.size()-1));
+
 		*/
 	}
 
@@ -36,6 +43,8 @@ public class CompraServiceImp implements ICompraService{
 	 
 	 @Override
 	public List<Compra> obtenerCompras() {
+		LOGGER.info("SERVICE: CompraServiceImp");
+		LOGGER.info("METHOD: obtenerCompras - se muestra la lista Compras -> "+ compras );
 		return compras;
 	}
 	 
