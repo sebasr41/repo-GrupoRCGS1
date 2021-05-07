cb8103f1a7f91c92696c56f4ae55d1dedb450713
sebas <sebas@LAPTOP-PALU9LV1>
2021-05-06 23:47:57 -0300

    task : agregando la Clase Compra Controller
    responsable : Sebastian Rojas

--- GRUPO_CRSG_TP5/src/main/java/ar/edu/unju/fi/tp4/controller/CompraController.java
+++ GRUPO_CRSG_TP5/src/main/java/ar/edu/unju/fi/tp4/controller/CompraController.java
@@ -1,5 +1,32 @@
 package ar.edu.unju.fi.tp4.controller;
 
-public class CompraController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import ar.edu.unju.fi.tp4.model.Compra;
+import ar.edu.unju.fi.tp4.service.ICompraService;
+
+@Controller
+public class CompraController {
+	@Autowired
+	 ICompraService compraService;
+	@Autowired
+	private Compra compra;
+		
+		@GetMapping("/compra")
+		public String getCompraPage(Model model) {
+			model.addAttribute(compra);
+			return "compras";
+		}
+		@PostMapping("compra/nuevo")
+		public String addCompraPage(@ModelAttribute("compra") Compra compra){
+			compraService.obtenerCompras();
+			return "nueva-compra";
+	}
+		
+	}
 }
