527618b4496964ca0adb83fd043e7ee53857c36a
sebas <sebas@LAPTOP-PALU9LV1>
2021-05-08 21:07:34 -0300

    task : compra controller

--- GRUPO_CRSG_TP5/src/main/java/ar/edu/unju/fi/tp4/controller/CompraController.java
+++ GRUPO_CRSG_TP5/src/main/java/ar/edu/unju/fi/tp4/controller/CompraController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.unju.fi.tp4.model.Compra;
 import ar.edu.unju.fi.tp4.service.ICompraService;
@@ -20,13 +21,16 @@ public class CompraController {
 		@GetMapping("/compra")
 		public String getCompraPage(Model model) {
 			model.addAttribute(compra);
-			return "compras";
-		}
-		@PostMapping("compra/nuevo")
-		public String addCompraPage(@ModelAttribute("compra") Compra compra){
-			compraService.obtenerCompras();
 			return "nueva-compra";
+		}
+		@PostMapping("/compra-guardar")
+		public ModelAndView addCompraPage(@ModelAttribute("compra") Compra compra){
+			 
+			 ModelAndView modelView =  new ModelAndView("compras");
+			 compraService.guardarCompra(compra);
+			 modelView.addObject("compras", compraService.obtenerCompras());
+			 return modelView;
 	}
 		
 	}
-}
+
