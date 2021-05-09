12b3de11e7966d2f84f895aabf40dc695812fdc6
Cristian Michel <cristianmichel33@gmail.com>
2021-05-09 17:39:34 -0300

    Agregar codigo en el service Compra para que se complete con los datos de la lista producto si se encuentra con el codigo ingresado, modificacion del service de producto para que tenga un arraylist en util y se visualize la tabla de productos

--- GRUPO_CRSG_TP5/src/main/java/ar/edu/unju/fi/tp4/service/imp/CompraServiceImp.java
+++ GRUPO_CRSG_TP5/src/main/java/ar/edu/unju/fi/tp4/service/imp/CompraServiceImp.java
@@ -6,13 +6,16 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
 
+import ar.edu.unju.fi.tp4.model.Compra;
+import ar.edu.unju.fi.tp4.model.Producto;
 import ar.edu.unju.fi.tp4.service.ICompraService;
+import ar.edu.unju.fi.tp4.util.TablaProducto;
 
 @Service
 public class CompraServiceImp implements ICompraService{
 	
 	private static final Log LOGGER = LogFactory.getLog(CompraServiceImp.class);
-	//private List<Compra> compras;
+	private List<Compra> compras;
 
 	@Override
 	public void generarTablaCompra() {
@@ -26,13 +29,27 @@ public class CompraServiceImp implements ICompraService{
 	//agregar Compra
 	@Override
 	public void guardarCompra(Compra compra) {
-		// TODO Auto-generated method stub
+		LOGGER.info("SERVICE: CompraServiceImp");
+		LOGGER.info("Parametro: "+ compra);
+		
 		if(compras == null) {
-			generarTablaLCompra();
+			generarTablaCompra();
 		}
+		//Detecta si existe el producto segun el codigo ingresado
+		for (int x = 0; x < TablaProducto.listaProductos.size(); x++) {
+			Producto p = TablaProducto.listaProductos.get(x);
+			if (p.getCodigo() == compra.getProducto().getCodigo()) {
+				LOGGER.info("Service: Producto Encontrado");
+                compra.setProducto(p);
+				break; 
+			}
+		}
+		double total=compra.getCantidad()*compra.getProducto().getPrecio();
+		compra.setTotal(total);
+		LOGGER.info("total: " + total);
+		
 		compras.add(compra);
 		
-		LOGGER.info("SERVICE: CompraServiceImp");
 		LOGGER.info("METHOD: guardarCompra - se agregó un objeto compra en la lista -> " + compras.get(compras.size()-1));
 
 	}
